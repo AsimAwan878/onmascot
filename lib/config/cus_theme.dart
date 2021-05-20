2fc3afda841287bc6840963c511b2777faee637e
HanzalaS <hanzalasaeed47@gmail.com>
2021-05-19 23:08:02 -0700

    get started screen completed along with custom button widget

---
+++ lib/config/cus_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:onmascota/utils/constants.dart';
+
+ThemeData themeData = ThemeData(
+  primaryColor: primaryColor,
+  buttonColor: primaryColor,
+);
---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+sizeHeight(BuildContext context) {
+  double height = MediaQuery.of(context).size.height;
+  return height;
+}
+
+sizeWidth(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  return width;
+}
+
+Color primaryColor = Color(0xfffdb23d);
